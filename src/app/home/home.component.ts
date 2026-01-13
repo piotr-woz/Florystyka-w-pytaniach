edbd38bb997e212f9798bc9a19cdacc90cf9b760
piotr-woz <piotrwozniak.dev@gmail.com>
2026-01-13 15:51:05 +0100

    Refactor HeaderComponent styles from CSS to SCSS and update the template to improve consistency

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { QuizComponent } from '../quiz/quiz.component';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
