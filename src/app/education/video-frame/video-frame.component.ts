f6604999bd4032a1725a267826491b07d20d91de
piotr-woz <piotrwozniak.dev@gmail.com>
2026-02-18 00:25:23 +0100

    Refactor configuration files and improve video frame component logic (withComponentInputBinding)

--- src/app/education/video-frame/video-frame.component.ts
+++ src/app/education/video-frame/video-frame.component.ts
@@ -1,4 +1,12 @@
-import { Component, inject, OnInit, signal, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  ChangeDetectionStrategy,
+  input,
+  computed,
+} from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { SafePipe } from '../../safe.pipe';
 
@@ -7,28 +15,56 @@ import { SafePipe } from '../../safe.pipe';
   imports: [SafePipe, RouterLink],
   templateUrl: './video-frame.component.html',
   styleUrl: './video-frame.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class VideoFrameComponent implements OnInit {
-  protected readonly videoUrl = signal<string | null>(null);
+export default class VideoFrameComponent {
+  // *** ActivatedRoute version ***
+  // protected readonly videoUrl = signal<string | null>(null);
 
-  private route = inject(ActivatedRoute);
+  // private readonly route = inject(ActivatedRoute);
+  // private readonly YT_ID_REGEX = /^[a-zA-Z0-9_-]{11}$/;
+
+  // ngOnInit(): void {
+  //   this.route.queryParams.subscribe(params => {
+  //     const id = params['id'];
+
+  //     if (this.isValidYoutubeId(id)) {
+  //       this.videoUrl.set(`https://www.youtube.com/embed/${id}?si=Denm8vm_D5_7nnfW`);
+  //     } else {
+  //       console.warn('Invalid video id', id);
+  //       this.videoUrl.set(null);
+  //     }
+  //   });
+  // }
+
+  // private isValidYoutubeId(id: unknown): id is string {
+  //   return typeof id === 'string' && this.YT_ID_REGEX.test(id);
+  // }
+
+  // *** withComponentInputBinding version ***
+  // withComponentInputBinding allows us to use the input() function to bind route parameters directly to component properties
+  // Here, we bind the 'id' query parameter to the videoId signal
+
+  // Takes the 'id' query parameter from the route and assigns it to the videoId signal
+  protected readonly videoId = input<string | null>(null, { alias: 'id' });
+
   private readonly YT_ID_REGEX = /^[a-zA-Z0-9_-]{11}$/;
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      const id = params['id'];
+  // Automatically compute the video URL whenever the videoId changes
+  protected readonly videoUrl = computed(() => {
+    const id = this.videoId();
 
-      if (this.isValidYoutubeId(id)) {
-        this.videoUrl.set(`https://www.youtube.com/embed/${id}?si=Denm8vm_D5_7nnfW`);
-      } else {
-        console.warn('Invalid video id', id);
-        this.videoUrl.set(null);
-      }
-    });
-  }
+    if (this.isValidYoutubeId(id)) {
+      return `https://www.youtube.com/embed/${id}?si=Denm8vm_D5_7nnfW`;
+    }
 
-  private isValidYoutubeId(id: unknown): id is string {
-    return typeof id === 'string' && this.YT_ID_REGEX.test(id);
+    if (id) {
+      console.warn('Invalid video id:', id);
+    }
+    return null;
+  });
+
+  private isValidYoutubeId(id: string | null): id is string {
+    return !!id && this.YT_ID_REGEX.test(id);
   }
 }
